adffb443ca8f1198d7725cee5075cc9267369066
Nirupama Nair <94469160+Nirupama03@users.noreply.github.com>
2024-02-22 23:42:32 +0530

    Delete S6 AI/ailabtrials directory

--- S6 AI/ailabtrials/trial3.py
+++
@@ -1,9 +0,0 @@
-#write a numpy pgm to reverse an array
-
-import numpy as py
-
-l=[1,2,3,4,5]
-arr=py.array(l)
-x = py.flip(arr)
-print("The reversed array is :")
-print(x)
--- S6 AI/ailabtrials/zdf.py
+++
@@ -1,4 +0,0 @@
-#write a python pgm to make a nested list and also concatenate list
-#write a numpy pgm to convert a list numeric value into a one dimensional numpy arrayu
-#write a numpy pgm to reverse an array
-#write a numpy pgm to capitalize  the first letter , lowercase , uppercase, titlecase of all the elements of the given array
